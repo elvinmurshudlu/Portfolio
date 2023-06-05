5b3e6d8b0dccfaf97bfa1e901864329ef4d7ee44
elvinmurshudlu <murshudlu.elvin@gmail.com>
2023-06-06 01:02:51 +0400

    Delete IMG

--- src/components/Introduce.tsx
+++ src/components/Introduce.tsx
@@ -3,8 +3,12 @@ import profile from "../assets/images/Profile.png"
 import status from "../assets/images/Status.svg"
 import Typewriter from 'typewriter-effect';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PATH } from '../constants/paths';
 
 export default function Introduce() {
+
+    const navigate = useNavigate()
 
     const [active,setActive] = useState(false)
     const [cursor,setCursor] = useState("")
@@ -61,15 +65,15 @@ export default function Introduce() {
 
 
                 <Box>
-                <Button variant='text' sx={{border:"1px solid #C778DD ",color:"white",padding:"8px 16px"}}>Contact me</Button>
+                <Button onClick={()=>navigate(PATH.aboutme)} variant='text' sx={{border:"1px solid #C778DD ",color:"white",padding:"8px 16px"}}>About me</Button>
 
                 </Box>
 
             </Grid>
-            <Grid item xs={12} md={6} sx={{display:"flex",flexDirection:"column",justifyContent:"center"}}>
+            {/* <Grid item xs={12} md={6} sx={{display:"flex",flexDirection:"column",justifyContent:"center"}}>
                 <img src={profile}></img>
                 <Box sx={{padding:"5px",border:"1px solid white",color:"#ABB2BF",display:"flex",alignItems:"center",columnGap:"5px"}}><img src={status}></img> Currently working on <Box component="span" color="white">Portfolio</Box></Box>
-            </Grid>
+            </Grid> */}
         </Grid>
   )
 }
